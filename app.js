f47c2bc26738bed07a0bca0541ae963af41fe316
Cristian Sánchez <cristian.sanz@gmail.com>
2021-07-01 20:46:23 -0300

    prueba error

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const path = require('path');
 
 const app = express();
 
-app.get('/', (req,res) =>{
-   res.sendFile(path.resolve(__dirname, './views/home.html'));  // Permite enviar un archivo HTML
-});
-
 app.use(express.static(path.resolve(__dirname, './public')));  
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor corriendo");
 });
+
+app.get('/', (req,res) =>{
+   res.sendFile(path.resolve(__dirname, './views/home.html'));  // Permite enviar un archivo HTML
+});
+
+
 
 
 
