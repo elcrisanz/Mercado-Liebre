0e825226d420e2d6ac986dab9d3507a8f4b91e37
Cristian Sánchez <cristian.sanz@gmail.com>
2021-07-09 21:55:23 -0300

    Corrigo Header | sumo login y register

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, './public')));  
+app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, './views')));  
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor corriendo");
@@ -13,7 +14,13 @@ app.get('/', (req,res) =>{
    res.sendFile(path.resolve(__dirname, './views/home.html'));  // Permite enviar un archivo HTML
 });
 
+app.get('/register', (req,res) =>{
+    res.sendFile(path.resolve(__dirname, './views/register.html'));  // Permite enviar un archivo HTML
+ });
 
+ app.get('/login', (req,res) =>{
+    res.sendFile(path.resolve(__dirname, './views/login.html'));  // Permite enviar un archivo HTML
+ });
 
 
 
